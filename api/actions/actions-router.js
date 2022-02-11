33347c30161200d4448e41d19c9c54a21f192e13
bugsbrog <bugsbrog@gmail.com>
2022-02-11 11:54:16 -0700

    Made both routers async

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -2,23 +2,23 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
 
 })
 
-router.get('/:id', (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
 
 })
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
 
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', async (req, res, next) => {
 
 })
 
-router.delete(':/id', (req, res, next) => {
+router.delete('/:id', async (req, res, next) => {
 
 })
 
