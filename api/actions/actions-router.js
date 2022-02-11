e39d5f086827dc1cd7e896b7ed88ad6e1c53aadc
bugsbrog <bugsbrog@gmail.com>
2022-02-11 11:58:49 -0700

    Got try catch set up on routers

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -3,23 +3,46 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 router.post('/', async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.put('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 router.delete('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 module.exports = router
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -3,27 +3,55 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 router.post('/', async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.put('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 router.delete('/:id', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 router.get('/:id/actions', async (req, res, next) => {
+    const { id } = req.params
+        try {
 
+        } catch (err) {
+            next(err)
+        }
 })
 
 module.exports = router
